9d31b8f00f7f856f39a21a3681b682b9d70d3e83
FernandoMoyano <fernandomoyano21@gmail.com>
2023-10-17 15:59:50 -0300

    userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,13 @@ const bccrypt = require("bccrypt");
  * @route GET /users
  * @access Private
  */
-const getAllUsers = asyncHandler(async (req, res) => {});
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().select("-password").lean();
+  if (!users) {
+    return res.status(400).json({ message: "No users found" });
+  }
+  res.json(users);
+});
 
 /**
  * Description Create new user
@@ -19,7 +25,21 @@ const createNewUser = asyncHandler(async (req, res) => {});
 
 /**
  * Description Update a user
- * @route PATCH /users
+ * @route PATCH /user
  * @access Private
  */
 const updateUser = asyncHandler(async (req, res) => {});
+
+/**
+ * Description Delete a user
+ * @route DELETE /user
+ * @access Private
+ */
+const deleteUser = asyncHandler(async (req, res) => {});
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+};
