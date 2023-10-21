35a72b79336dfee70e2c6a5a0780a809044d9027
FernandoMoyano <fernandomoyano21@gmail.com>
2023-10-21 00:14:25 -0300

    components-Layout-Public

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 function App() {
- 
   return (
-    
-      <div>Hola Mundo</div>
-        
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
