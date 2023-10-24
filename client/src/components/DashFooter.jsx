9ad838ce0aea5e2620de179016b89cb0a004c87b
FernandoMoyano <fernandomoyano21@gmail.com>
2023-10-24 15:14:18 -0300

    DashFooter1

--- client/src/components/DashFooter.jsx
+++ client/src/components/DashFooter.jsx
@@ -1,10 +1,28 @@
-import { fontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { useNavigation, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const DashFooter = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const onGoHomeClicked = () => navigate("/dash");
+
+  let goHomeButton = null;
+  if (pathname !== "/dash") {
+    goHomeButton = (
+      <button
+        className="dash-footer__button icon-button"
+        title="Home"
+        onClick={onGoHomeClicked}
+      >
+        <FontAwesomeIcon icon={faHouse} />
+      </button>
+    );
+  }
+
   const content = (
     <footer className="dash-footer">
+      {goHomeButton}
       <p>Usuario actual:</p>
       <p>Estado</p>
     </footer>
