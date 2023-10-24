a0a1cb0264d33ea213009262612f4d66e6412eab
FernandoMoyano <fernandomoyano21@gmail.com>
2023-10-24 13:56:14 -0300

    Login-DashLayout-DashHeader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./components/Login";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}></Route>
+      <Route index element={<Public />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   );
 }
--- client/src/components/Public.jsx
+++ client/src/components/Public.jsx
@@ -1,9 +1,36 @@
-
+import { Link } from "react-router-dom";
 
 const Public = () => {
-  return (
-    <div>Public</div>
-  )
-}
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Bienvenido a <span className="nowrap">Juan D.¡Reparaciones!</span>
+        </h1>
+      </header>
+      <main className="public__main">
+        <p>
+        Ubicado en el hermoso centro de Córdoba, Juan D. Reparaciones ofrece un
+        Personal capacitado listo para satisfacer sus necesidades de reparación tecnológica.
+        </p>
+        <address className="public__addr">
+        Juan D.¡Reparaciones!
+          <br />
+          555 San Martín
+          <br />
+          Córdoba Capital-Argentina
+          <br />
+          <a href="tel:+15555555555">(555) 555-5555</a>
+        </address>
+        <br />
+        <p>Dueño : Juan D</p>
+      </main>
+      <footer>
+        <Link to="/login">Inicio de Sesion</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
 
-export default Public
+export default Public;
