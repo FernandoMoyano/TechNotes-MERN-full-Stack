3f002d79797a12318ed9c40f5702971c14b39570
FernandoMoyano <fernandomoyano21@gmail.com>
2023-10-24 18:40:09 -0300

    app-imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,27 @@ import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
-      <Route index element={<Public />}></Route>
-      <Route path="login" element={<Login />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />}></Route>
+        <Route path="login" element={<Login />}></Route>
 
-      <Route path="dash" element={<DashLayout />}>
-        <Route index element={<Welcome/>}/>
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />}></Route>
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />}></Route>
+          </Route>
+        </Route>
       </Route>
     </Routes>
   );
