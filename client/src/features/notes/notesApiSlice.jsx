011944628538d645fbab0fbcc86249f268682096
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-01 19:12:56 -0300

    UserList

---
+++ client/src/features/notes/notesApiSlice.jsx
@@ -0,0 +1,53 @@
+import { createSelector, createEntityAdapter } from "reduxjs/toolkit";
+import { apiSlice } from "../../app/api/apiSlice";
+
+const notesAdapter = createEntityAdapter({});
+const initialState = notesAdapter.getInitialState();
+
+export const notesApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getnote: builder.query({
+      query: () => "/note",
+      validateStatus: (response, result) => {
+        return response.status === 200 && !result.isError;
+      },
+      keepUnusedDataFor: 5,
+      transformResponse: (responseData) => {
+        const loadedNotes = responseData.map((user) => {
+          user.id = user._id;
+          return user;
+        });
+        return notesAdapter.setAll(initialState, loadedNotes);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "User", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "User", id })),
+          ];
+        } else return [{ type: "User", id: "LIST" }];
+      },
+    }),
+  }),
+});
+
+export const { useGetNoteQuery } = notesApiSlice;
+
+// returns the query result object
+export const selectNotesResult = notesApiSlice.endpoints.getnote.select();
+
+// creates memoized selector
+const selectNotesData = createSelector(
+  selectNotesResult,
+  (notesResult) => notesResult.data // normalized state object with ids & entities
+);
+
+//getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+  selectAll: selectAllNote,
+  selectById: selectNoteById,
+  selectIds: selectNserIds,
+  // Pass in a selector that returns the note slice of state
+} = notesAdapter.getSelectors(
+  (state) => selectNotesData(state) ?? initialState
+);
