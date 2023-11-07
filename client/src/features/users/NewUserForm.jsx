9c57a913ebf7f27ac58598d803c1b56a2fb42d50
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-06 21:45:58 -0300

    NewUseForm-states

--- client/src/features/users/NewUserForm.jsx
+++ client/src/features/users/NewUserForm.jsx
@@ -5,10 +5,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
 
-const USER_REGEX = /^[A-z]{3,20}$/
-const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
+const USER_REGEX = /^[A-z]{3,20}$/;
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
 const NewUserForm = () => {
+  const [addNewUser, { isLoading, isSuccess, isError, error }] =
+    useAddNewUserMutation();
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [validUsername, setValidUsername] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [roles, setRoles] = useState(["Employee"]);
+
   return <div>NewUserForm</div>;
 };
 
