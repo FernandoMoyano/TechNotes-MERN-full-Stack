895e216e800d5fe1c477eaeb378b88ef6bf34f66
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-10 12:46:27 -0300

    EditUserForm1

--- client/src/features/users/EditUserForm.jsx
+++ client/src/features/users/EditUserForm.jsx
@@ -1,8 +1,40 @@
+import { useState, useEffect } from "react";
+import { useUpdateUserMutation, useDeleteUserMutation } from "./UsersApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { ROLES } from "../../config/roles";
 
-const EditUserForm = () => {
-  return (
-    <div>EditUserForm</div>
-  )
-}
+const USER_REGEX = /^[A-z]{3,20}$/;
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
-export default EditUserForm
+const EditUserForm = ({ user }) => {
+  const [updateUser, { isLoading, isSuccess, isError, error }] =
+    useUpdateUserMutation();
+
+  const [
+    deleteUser,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteUserMutation();
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState(user.username);
+  const [validUsername, setValidUsername] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [roles, setRoles] = useState(user.roles);
+  const [active, setActive] = useState(user.active);
+
+  useEffect(() => {
+    setValidUsername(USER_REGEX.test(username));
+  }, [username]);
+
+  useEffect(() => {
+    setValidPassword(PWD_REGEX.test(password));
+  }, [password]);
+
+  return <div>EditUserForm</div>;
+};
+
+export default EditUserForm;
