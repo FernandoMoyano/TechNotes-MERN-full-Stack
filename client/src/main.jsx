9a4f5002bc9f97a40b9b7a1154a1b12cab90540a
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-20 16:44:01 -0300

    notes/NewNote

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { store } from "./App/store";
+import { store } from "./app/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
